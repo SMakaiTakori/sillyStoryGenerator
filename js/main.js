a6454332de83780fb9923cba099ffd6f356e75c4
SMakaiTakori <makait87@icloud.com>
2019-06-25 17:10:25 -0400

    added new variables and completed functions

--- js/main.js
+++ js/main.js
@@ -6,8 +6,39 @@ function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
 
-var storyText = `It was 94 fahrenheit outside, so ${insertx} went for a walk. When they got to ${inserty}, they stared in horror for a few moments, then ${insertz}. Bob saw the whole thing, but was not surprised — ${insertx} weighs 300 pounds, and it was a hot day.`
-var insertx = ['Willy the Goblin, Big Daddy, Father Christmas']
-var inserty = ['the soup kitchen, Disneyland, the White House']
-var insertz = ['spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away']
+var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+var storyText = `It was 94 fahrenheit outside, so ${insertX} went for a walk. When they got to ${insertY}, they stared in horror for a few moments, then ${insertZ}. Bob saw the whole thing, but was not surprised — ${insertX} weighs 300 pounds, and it was a hot day.`;
 
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  var newStory = storyText;
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(insertX, xItem);
+  newStory = newStory.replace(insertX, xItem);
+  newStory = newStory.replace(insertY, yItem);
+  newStory = newStory.replace(insertZ, zItem);
+
+  if(customName.value !== '') {
+    var name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  } 
+
+  if(document.getElementById("uk").checked) {
+    var weight = Math.round(300/14) + ' stones';
+    var temperature =  Math.round((94-32)*5/9) + ' centigrade';
+
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
+  }
+  
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
